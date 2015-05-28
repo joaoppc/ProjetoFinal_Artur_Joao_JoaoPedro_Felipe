e14fdc60fe89f0c7b0c853301bd8618df0a3c4ff
febuniac <febuniac@gmail.com>
2015-05-28 10:41:41 -0300

    INÍCIO
    Criação de Classes com as listas do indivíduo vazia e a lista de
    atribuição de cada categoria

---
+++ Projeto Final.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 09:02:38 2015
+
+@author: Felipe
+"""
+class Person:
+    def __init__(self,Esporte_ind,Preferencia_ind,Música_ind,Instrumentos_ind,Entidades_ind):
+        self.Esporte_ind =[]
+        self.Preferencia_ind =[]
+        self.Música_ind =[]
+        self.Instrumentos_ind = []
+        self.Entidades_ind =[]
+
+Esporte = ["Futebol","Futsal","Vôlei","Handebol","Jiu-Jitsu","Judô","Rugby","Natação","Basquete","Tênis de Mesa","Tênis","Xadrez"]
+Preferencia = ["Balada","Ler um Livro","Role no Shopping","Ficar em Casa","Soneca","Passear no Parque","Concerto","Tatro","Show","Bar","Cinema","Stand-Up"]
+Música = ["Eletrônica","Reggae","Rock","Rap","Sertanejo","Samba","Pagode","Clássica","Funk","Jazz","Forró","MPB"]
+Instrumentos = ["Percurssão","Saxofone","Violão","Gaita","Piano","Baixo","Banjo","Cavaquinho","Xilofone","Violino","Flauta","Ukulele"]
+Entidade= ["Bateria Imperial","Revista Insper Post","Liga de Enpreendendorismo","Sementes Culturais","Diretório Acâdemico","InFinance","AIESEC","Enactus","BemGasto","GAS","Atlética","Insper Jr"]
